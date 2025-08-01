22cb1c7267b9cb5c3c3043876bc6a48afe460acf
NikhilPatel003 <ntp170405@gmail.com>
2025-08-01 23:56:13 +0530

    Created WatchList Component

--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,4 +1,12 @@
-import React from "react";
+import { Tooltip, Grow } from "@mui/material";
+import { watchlist } from "../data/data";
+import { useState } from "react";
+import {
+  KeyboardArrowDown as ArrowDownIcon,
+  KeyboardArrowUp as ArrowUpIcon,
+  BarChart as BarChartIcon,
+  MoreHoriz as MoreIcon,
+} from "@mui/icons-material";
 
 const WatchList = () => {
   return (
@@ -11,12 +19,85 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, idx) => {
+          return <WatchListItem key={idx} stock={stock} />;
+        })}
+      </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchListActions, setShowWatchListActions] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setShowWatchListActions(true);
+  };
+  const handleMouseExit = (e) => {
+    setShowWatchListActions(false);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <ArrowDownIcon className="down" />
+          ) : (
+            <ArrowUpIcon className="up" />
+          )}
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {showWatchListActions && <WatchListActions uid={stock.name} />}
+    </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  return (
+    <span className="actions">
+      <span>
+        <Tooltip
+          title="Buy (B)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="buy">Buy</button>
+        </Tooltip>
+        <Tooltip
+          title="Sell (S)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="sell">Sell</button>
+        </Tooltip>
+        <Tooltip
+          title="Analytics (A)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="action">
+            <BarChartIcon className="icon" />
+          </button>
+        </Tooltip>
+        <Tooltip title="More" placement="top" arrow TransitionComponent={Grow}>
+          <button className="action">
+            <MoreIcon className="icon" />
+          </button>
+        </Tooltip>
+      </span>
+    </span>
+  );
+};
